f312b316433b1539d7131a709b594a9194387547
Maaz Shah <dev@192.168.10.7>
2022-08-09 23:42:45 +0500

    final commit

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,25 +1,78 @@
-import { useState } from 'react';
-
+import { useState, useRef, useContext } from 'react';
+import { useHistory } from 'react-router-dom'
+import AuthContext from '../../store/auth-context.js'
 import classes from './AuthForm.module.css';
 
 const AuthForm = () => {
+  const authCtx = useContext(AuthContext);
+  const history = useHistory()
+
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
   const [isLogin, setIsLogin] = useState(true);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const emailValue = emailRef.current.value;
+    const passwordValue = passwordRef.current.value;
+
+    let url = ''
+    if(isLogin){
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyD-PjGnJ-TGyGPQg4jko0vs9Ekdi6XtznA'
+    }
+    else {
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyD-PjGnJ-TGyGPQg4jko0vs9Ekdi6XtznA'
+    }
+    fetch(url,
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          email: emailValue,
+          password: passwordValue,
+          returnSecureToken: true
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    ).then(res => {
+      if(res.ok) {
+        return res.json()
+      }
+      else {
+        // console.log(res)
+        return res.json().then(data => {
+          let errorMessage = 'Authentication Failed!!!'
+          throw new Error(errorMessage)
+          // console.log(data)
+        })
+      }
+    }).then(data => {
+      authCtx.login(data.idToken)
+      history.replace('/')
+    })
+    .catch(err => {
+      alert(err.message);
+    });
+  };
+
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <input type='email' id='email' ref={emailRef} required />
         </div>
         <div className={classes.control}>
           <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <input type='password' id='password' ref={passwordRef} required />
         </div>
         <div className={classes.actions}>
           <button>{isLogin ? 'Login' : 'Create Account'}</button>
--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,11 +1,39 @@
+import React, { useRef, useContext } from 'react'
+import { useHistory } from 'react-router-dom'
+import AuthContext from '../../store/auth-context';
 import classes from './ProfileForm.module.css';
 
 const ProfileForm = () => {
+  const history = useHistory()
+  const authCtx = useContext(AuthContext)
+  const newPasswordInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const newPassword = newPasswordInputRef.current.value;
+
+    fetch('https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyD-PjGnJ-TGyGPQg4jko0vs9Ekdi6XtznA', {
+      method: 'POST',
+      body: JSON.stringify({
+        idToken: authCtx.token,
+        password: newPassword,
+        returnSecureToken: false
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res => {
+      history.replace('/')
+      console.log(res)
+    })
+  }
+
   return (
-    <form className={classes.form}>
+    <form onSubmit={submitHandler} className={classes.form}>
       <div className={classes.control}>
         <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <input type='password' ref={newPasswordInputRef} minLength="7" id='new-password' />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
